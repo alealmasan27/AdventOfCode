3f2f38cae8b1b59434791fbfcb46c193789b377e
Alexandra Almasan <alexandra_almasan27@yahoo.com>
2020-12-16 19:59:51 +0200

    Day15: Improved performance
    Signed-off-by: Alexandra Almasan <alexandra_almasan27@yahoo.com>

--- Day15/Day15.cs
+++ Day15/Day15.cs
@@ -6,42 +6,39 @@ namespace AdventOfCode.Day15
 {
     internal class Day15
     {
-        private List<long> _inputs;
+        private readonly List<long> _inputs;
         private Dictionary<long, long> _lastAppearancePosition;
 
-        private void Initializations()
+        public Day15()
         {
-            _inputs = new List<long>();
+            _inputs = new List<long>(File.ReadAllLines("Day15.txt")[0].Split(',').Select(long.Parse));
+        }
+
+        private void DictionaryInitialization()
+        {
             _lastAppearancePosition = new Dictionary<long, long>();
-            ReadData();
-            for (var i = 0; i < _inputs.Count - 1; i++)
+            for (var i = 0; i < _inputs.Count - 1; i++) //without the last element of the list
                 _lastAppearancePosition.Add(_inputs[i], i + 1);
         }
 
-        private void ReadData()
-        {
-            _inputs.AddRange(File.ReadAllLines("Day015.txt")[0].Split(',').Select(long.Parse));
-        }
-
-
         public long NthNumberSpoken(long nthNumberSpokenIndex)
         {
-            Initializations();
+            DictionaryInitialization();
+            var lastNumberSpoken = _inputs[^1];
             for (var i = _inputs.Count; i < nthNumberSpokenIndex; i++)
             {
-                if (_lastAppearancePosition.TryGetValue(_inputs[i - 1], out var index))
+                if (_lastAppearancePosition.TryGetValue(lastNumberSpoken, out var index))
                 {
-                    _lastAppearancePosition[_inputs[i - 1]] = i;
-                    _inputs.Add(i - index);
+                    _lastAppearancePosition[lastNumberSpoken] = i;
+                    lastNumberSpoken = i - index;
                 }
                 else
                 {
-                    _lastAppearancePosition.Add(_inputs[i - 1], i);
-                    _inputs.Add(0);
+                    _lastAppearancePosition.Add(lastNumberSpoken, i);
+                    lastNumberSpoken = 0;
                 }
             }
-
-            return _inputs[^1];
+            return lastNumberSpoken;
         }
     }
 }
